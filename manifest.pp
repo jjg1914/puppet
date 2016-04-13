01f8972c319cda75a6f893743a897ffea1bd98c5
John J. Glynn IV <jjg1914@rit.edu>
2016-04-13 14:31:25 -0400

    sudo setup

--- manifest.pp
+++ manifest.pp
@@ -7,14 +7,42 @@ node jglynn-arch {
   
   service { "sshd": ensure => running }
 
+  group { "sudo":
+    ensure => present,
+    system => true,
+  }
+
+  file { "/tmp/sudoers":
+    content => "%sudo	ALL=(ALL) NOPASSWD: ALL",
+    owner => "root",
+    group => "root",
+    mode => "0440",
+    require => Group["sudo"],
+  }
+
+  exec { "sudoers":
+    command => "/usr/bin/visudo -cf /tmp/sudoers",
+    require => File["/tmp/sudoers"]
+  }
+
+  file { "/etc/sudoers":
+    source => "file:///tmp/sudoers",
+    owner => "root",
+    group => "root",
+    mode => "0440",
+    require => Exec["sudoers"],
+  }
+
   user { "jglynn":
     ensure => present,
     uid => 1000,
     gid => "users",
+    groups => "sudo",
     shell => "/bin/bash",
     home => "/home/jglynn",
     comment => "John J. Glynn IV",
     managehome => true,
+    require => Group["sudo"],
   }
 
   file { "/home/jglynn":
@@ -22,6 +50,6 @@ node jglynn-arch {
     owner => "jglynn",
     group => "users",
     mode => "0750",
-    require => [ User["jglynn"] ],
+    require => User["jglynn"],
   }
 }
